78aab43f5444c900e79946583d75db9060e501c3
Guilherme <guilherme.soares@linea.gov.br>
2017-07-05 15:09:14 -0300

    [S7]:titulo a esquerda, logo DES, sem search, logo footer maior

---
+++ wp-content/themes/science-server/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+?>
+
+		</div><!-- #main -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<?php get_sidebar( 'main' ); ?>
+
+			<div class="site-info">
+				<a href="http://www.linea.gov.br">Powered by
+					<img src="<?php echo get_site_url() . '/wp-content/uploads/2017/07/logo-header.png' ?>">
+				</a>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
+	</div><!-- #page -->
+
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/science-server/index.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		<?php if ( have_posts() ) : ?>
+
+			<?php // Retrieve only Pages and exclude About, links, contact, etc.
+				$args = array(       // set up arguments
+					'post_type' => 'page'
+		   	);
+				query_posts($args);   // execute the arguments
+			?>
+
+			<?php /* The loop */ ?>
+			<ul class="tool-box-container">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+						<?php
+							global $post;
+							$slug = $post->post_name;
+						?>
+						<li class="tool-box <?php echo $slug; ?>">
+								<a href="<?php the_permalink(); ?>">
+									<div class="tool-box-img">
+					        	<?php the_post_thumbnail(); ?>
+									</div>
+									<h2 class="tool-box-title"><?php the_title(); ?></h2>
+								</a>
+				    </li>
+					<?php endif; ?>
+				<?php endwhile; ?>
+			</ul>
+			<div class="clearboth"></div>
+
+
+			<?php twentythirteen_paging_nav(); ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
