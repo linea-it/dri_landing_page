d36a97aa0e3a1ee56557c72045426eb3ddc98b54
Guilherme <guilherme.soares@linea.gov.br>
2017-08-28 15:48:54 -0300

    verifica usuario logado

--- wp-content/themes/science-server/header.php
+++ wp-content/themes/science-server/header.php
@@ -44,7 +44,8 @@
 					<button class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></button>
 					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
+                    <span class="user-login"></span>
+                </nav><!-- #site-navigation -->
 			</div><!-- #navbar -->
 		</header><!-- #masthead -->
 
